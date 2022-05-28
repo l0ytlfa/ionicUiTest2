570caa06c6e6801a9309c311525d052c17ac3de0
Luka <luca.farina@fiorital.com>
2022-05-28 15:31:09 +0200

    modifica stili CSS globali per action carts

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,46 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public actionSheetController: ActionSheetController) {}
+
+  async goToCarts(){
+
+    const actionSheet = await this.actionSheetController.create({
+      header: 'I miei carrelli',
+      cssClass: 'my-custom-class',
+      buttons: [{
+        text: 'Sushi & co',
+        icon: 'cart',
+        id: 'delete-button',
+        cssClass: 'cartButtonAction',   //<--- NB: style must be in global.scss to avoid CSS encapsulation
+        data: {
+          type: 'delete'
+        },
+        handler: () => {
+          //--> go here to specific cart
+        }
+      }, {
+        text: 'DYP e confezionato',
+        icon: 'cart',
+        cssClass: 'cartButtonAction',
+        data: 10,
+        handler: () => {
+          //--> go here to specific cart
+        }
+      }, {
+        text: 'indietro',
+        icon: 'close',
+
+        role: 'cancel',
+        handler: () => {
+          //--> just close
+        }
+      }]
+    });
+    await actionSheet.present();
+
+    const { role, data } = await actionSheet.onDidDismiss();
+    console.log('onDidDismiss resolved with role and data', role, data);
+  }
 
 }
