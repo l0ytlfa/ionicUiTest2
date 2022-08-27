3e9873a3d8dc08da9a9283ed45e221204353e687
Luka <luca.farina@fiorital.com>
2022-08-27 14:05:56 +0200

    show / hide buttons dynamically

--- src/app/popupnews/popupnews.page.ts
+++ src/app/popupnews/popupnews.page.ts
@@ -1,3 +1,5 @@
+/* eslint-disable max-len */
+/* eslint-disable @typescript-eslint/semi */
 /* eslint-disable @typescript-eslint/quotes */
 /* eslint-disable one-var */
 /* eslint-disable @typescript-eslint/no-unused-expressions */
@@ -15,6 +17,9 @@ SwiperCore.use([Parallax]);
 export class PopupnewsPage implements OnInit {
 
   @ViewChild('swiper') swiperRef: any;
+  @ViewChild('SPREV') sprev: any;
+  @ViewChild('SNEXT') snext: any;
+
 
   o: any;
   l: any;
@@ -34,10 +39,18 @@ export class PopupnewsPage implements OnInit {
       , v = u.find(".fashion-slider-scale")
       , m = u.find("img")
       , g = f.find("img")
-      , p = n - s
-      ,y = v.attr("data-slide-bg-color");
+      , p = n - s;
 
-    a.css("background-color", y),
+    let averageRgb;
+    let averageHex;
+
+    //--> get average color from next slide image.....
+    if(g[0] !== undefined){
+      averageRgb = this.getAverageRGB(g[0]);
+      averageHex = this.rgbToHex(averageRgb.r,averageRgb.g,averageRgb.b);
+    }
+
+    a.css("background-color", averageHex),
     v.transform("scale(0.6)"),
     m.transition(1e3).transform("scale(1.2)"),
     u.find(".fashion-slider-title-text").transition(1e3).css("color", "rgba(255,255,255,0)"),
@@ -63,6 +76,10 @@ export class PopupnewsPage implements OnInit {
         this.o = !1;
     });
 
+    //---> manage buttons hide / show
+    s === 0 ? this.sprev.el.classList.add('hidePrevNextButton') : this.sprev.el.classList.remove('hidePrevNextButton'),
+    s === r.length - 1 ? this.snext.el.classList.add('hidePrevNextButton') : this.snext.el.classList.remove('hidePrevNextButton');
+
   }
 
   initswiper(e){
@@ -70,9 +87,6 @@ export class PopupnewsPage implements OnInit {
 
     //--> stop for opening the reset of state of image of fist slide (scale, ...)
     this.stopAnimations(n);
-
-    const a = r.eq(s).attr('data-slide-bg-color');
-    n.css('background-color', a),
     e.emit('transitionEnd');
 
   }
@@ -86,6 +100,64 @@ export class PopupnewsPage implements OnInit {
         this.o = !1;
     }
     );
+}
+
+
+//------> dynamic color
+
+componentToHex(c) {
+  const hex = c.toString(16);
+  return hex.length === 1 ? "0" + hex : hex;
+}
+
+rgbToHex(r, g, b) {
+  return "#" + this.componentToHex(r) + this.componentToHex(g) + this.componentToHex(b);
+}
+
+getAverageRGB(imgEl) {
+
+  let blockSize = 5, // only visit every 5 pixels
+      defaultRGB = {r:0,g:0,b:0}, // for non-supporting envs
+      canvas = document.createElement('canvas'),
+      context = canvas.getContext && canvas.getContext('2d'),
+      data, width, height,
+      i = -4,
+      length,
+      rgb = {r:0,g:0,b:0},
+      count = 0;
+
+  if (!context) {
+      return defaultRGB;
+  }
+
+  height = canvas.height = imgEl.naturalHeight || imgEl.offsetHeight || imgEl.height;
+  width = canvas.width = imgEl.naturalWidth || imgEl.offsetWidth || imgEl.width;
+
+  context.drawImage(imgEl, 0, 0);
+
+  try {
+      data = context.getImageData(0, 0, width, height);
+  } catch(e) {
+      /* security error, img on diff domain */
+      return defaultRGB;
+  }
+
+  length = data.data.length;
+
+  while ( (i += blockSize * 4) < length ) {
+      ++count;
+      rgb.r += data.data[i];
+      rgb.g += data.data[i+1];
+      rgb.b += data.data[i+2];
+  }
+
+  // ~~ used to floor values
+  rgb.r = Math.floor(rgb.r/count);
+  rgb.g = Math.floor(rgb.g/count);
+  rgb.b = Math.floor(rgb.b/count);
+
+  return rgb;
+
 }
 
 prevSlide(){
