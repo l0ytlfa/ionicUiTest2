6a09d143875fa514aaa491f28009d356e42dc735
Luka <luca.farina@fiorital.com>
2022-09-10 14:03:05 +0200

    update libs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,10 +15,9 @@ export function playerFactory() {
   return player;
 }
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,LottieModule.forRoot({ player: playerFactory })],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent],
+    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, LottieModule.forRoot({ player: playerFactory })],
+    providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+    bootstrap: [AppComponent]
 })
 export class AppModule {}
